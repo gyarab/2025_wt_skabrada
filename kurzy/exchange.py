c121b377ea24ec942e5835321b24137b01c09ae3
Tris4gone <simon.skabrada.s@gyarab.cz>
2026-01-20 15:38:24 +0100

    funkční kuzy a stahnutí venv

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,1 +1,7 @@
 import httpx
+
+c = httpx.get('https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt')
+print(c.text)
+
+lines = c.text.split('\n')
+print(lines[0])
