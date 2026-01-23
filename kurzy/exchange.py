76495c46293916748e9e49e89f8a7df7df208e17
Šimon Škabrada <simon.skabrada.s@gyarab.cz>
2026-01-23 10:18:47 +0100

    rozpoznaní eur v exchange

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -5,3 +5,20 @@ print(c.text)
 
 lines = c.text.split('\n')
 print(lines[0])
+
+line_euro = ""
+
+for line in lines:
+    if "EUR" in line:
+        line_euro = line
+        break
+
+rare_str = line_euro.split('|')[-1].replace(',' , ',')
+rate = float(rare_str)
+
+print("kurz eura je" , rate)
+
+value_in = float(input("kolik mas eur?"))
+value_out = value_in * rate
+
+print(f"tak to je {value_out:.2f} korun")
